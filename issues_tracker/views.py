5408f1871e4228f92979285fba4cca9a170e292b
mjeammet <marie.jeammet@protonmail.com>
2021-11-16 13:14:19 +0100

    nested routers

--- issues_tracker/views.py
+++ issues_tracker/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet, ViewSet
+from rest_framework import serializers
 
-from issues_tracker.models import Project, Issue, Comment
+from issues_tracker.models import User, Project, Issue, Comment
 from issues_tracker.serializers import IssueDetailSerializer, ProjectDetailSerializer, ProjectListSerializer, IssueListSerializer, CommentListSerializer
 
 # AUTHENTICATION RELATED CLASSES
-class SignUpViewset(ViewSet):
-    pass 
+# class SignUpView(ModelViewSet):
+class SignUpView(CreateAPIView):
+
+    def get_queryset(self):
+        return 
+
+    # def validate_username(self, value):
+    #     if User.objects.filter(username=value).exists():
+    #         raise serializers.ValidationError("Username already exists.")
+    #     return value
 
 
 class ProjectViewset(ReadOnlyModelViewSet):
@@ -30,10 +40,10 @@ class IssueViewset(ReadOnlyModelViewSet):
     detail_serializer_class = IssueDetailSerializer
 
     def get_queryset(self):
-        return Issue.objects.all()
+        return Issue.objects.filter(project_id=self.kwargs['project_pk'])
 
     def get_serializer_class(self):
-        if self.action == 'retrieve':
+        if self.action == 'retrieve':   
             return self.detail_serializer_class
         else:
             return super().get_serializer_class()
--- softdesk/urls.py
+++ softdesk/urls.py
@@ -3,22 +3,26 @@ from django.urls import path, include
 # from rest_framework import routers
 from rest_framework_nested import routers
 
-from issues_tracker.views import ProjectViewset, IssueViewset, CommentViewset
+from issues_tracker.views import ProjectViewset, IssueViewset, CommentViewset, SignUpView
 
-router = routers.SimpleRouter()
+router = routers.DefaultRouter()
 router.register('projects', ProjectViewset, basename='projects')
 
 issues_router = routers.NestedSimpleRouter(router, 'projects', lookup='project')
-issues_router.register('issues', IssueViewset, basename='project-issues')
+issues_router.register('issues', IssueViewset, basename='issues')
+
+comments_router = routers.NestedSimpleRouter(issues_router, 'issues', lookup='issue')
+comments_router.register('comments', CommentViewset, basename='comments')
 
 # router.register('issues', IssueViewset, basename='issues')
-router.register('comments', CommentViewset, basename='comments')
+# router.register('comments', CommentViewset, basename='comments')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    # path('signup/', SignUpViewset.as_view(), 'signup'),
+    path('signup/', SignUpView.as_view(), 'signup'),
     # path('login'),
     path('', include(router.urls)),
     path('', include(issues_router.urls)),
+    path('', include(comments_router.urls)),
 ]
